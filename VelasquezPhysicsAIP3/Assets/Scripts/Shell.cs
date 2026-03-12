345c22c40c8dd66191a6f2ccd99ad75986f4615d
Sara0614 <euniceisnot10@gmail.com>
2026-03-12 11:27:35 -0700

    Physics Ai Kinda done

--- VelasquezPhysicsAIP3/Assets/Scripts/Shell.cs
+++ VelasquezPhysicsAIP3/Assets/Scripts/Shell.cs
@@ -4,7 +4,17 @@ using UnityEngine;
 
 public class Shell : MonoBehaviour
 {
+ 
     public GameObject explosion;
+     float speed = 0f;
+    float yspeed = 0f;
+    float mass = 10;
+    float force = 2;
+    float drag = 1;
+    float gravity= -9.8f;
+    float gAccel;
+    float acceleration;
+
 
     void OnCollisionEnter(Collision col)
     {
@@ -19,12 +29,16 @@ public class Shell : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        acceleration = force / mass;
+        speed += acceleration * 1;
+        gAccel = gravity / mass;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-
+        speed *= (1 - Time.deltaTime * (drag));
+        yspeed += gAccel * Time.deltaTime;
+        this.transform.Translate(0, 0, yspeed * speed);
     }
 }
